e2420b587dbefa6eef63d4b1fde2f8e0b3a11aff
TecHeReTiC <talex73@mail.ru>
2022-07-08 05:22:03 +0500

    New tileset has been added.

--- levels/tilesets/random_colors_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="tileset" tilewidth="128" tileheight="128" tilecount="256" columns="16">
- <image source="../../../learning_openCV/scripts/tileset.jpg" width="2048" height="2048"/>
-</tileset>
