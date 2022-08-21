07cf8f764e450f0b3efeeb75363fa0ec04a2fe45
TecHeReTiC <talex73@mail.ru>
2022-08-21 22:19:32 +0500

    Current version of the game has been added.

--- output/Platformer/classes/game_manager.py
+++
@@ -1,6 +0,0 @@
-import pygame
-
-class GameManager:
-
-    def __init__(self):
-        self.game_state = 'game'
--- output/Platformer/levels/tilesets/mine_wood.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="mine_wood" tilewidth="128" tileheight="128" tilecount="36" columns="6">
- <image source="Wood_tileset.png" trans="000000" width="768" height="768"/>
-</tileset>
